77c0d89f9f05db26887e097ae594f5c1df40ce15
Angus Hollands <goosey15@gmail.com>
2020-04-10 13:55:14 +0100

    Remove unused imports, todo modify package.json

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,13 @@
 import {IRenderMimeRegistry} from '@jupyterlab/rendermime';
 import {markupRendererFactory} from "./factories";
-import {IMarkdownViewerTracker} from "@jupyterlab/markdownviewer"
-import {IEditorTracker} from "@jupyterlab/fileeditor"
 import {JupyterFrontEnd, JupyterFrontEndPlugin} from '@jupyterlab/application';
 
 
 const extension: JupyterFrontEndPlugin<void> = {
     id: '@agoose77/jupyterlab-markup',
     autoStart: true,
-    requires: [IRenderMimeRegistry, IMarkdownViewerTracker, IEditorTracker],
-    activate: (app: JupyterFrontEnd, registry: IRenderMimeRegistry,
-               markdownViewerTracker: IMarkdownViewerTracker,
-               editorTracker: IEditorTracker) => {
+    requires: [IRenderMimeRegistry],
+    activate: (app: JupyterFrontEnd, registry: IRenderMimeRegistry) => {
         console.log('JupyterLab extension @agoose77/jupyterlab-markup is activated!');
         registry.addFactory(markupRendererFactory);
     }
