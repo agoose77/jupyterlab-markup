984026b01f98f366aea57c3aa1589f443c86ff00
Angus Hollands <goosey15@gmail.com>
2021-05-04 09:28:23 +0100

    Feature: use jupyter_packaging backend

--- setup.py
+++ setup.py
@@ -1,95 +1,17 @@
-"""
-jupyterlab_markup setup
-"""
-import json
-import os
+#!/usr/bin/env python
+# coding: utf-8
 
-from jupyter_packaging import (
-    create_cmdclass, install_npm, ensure_targets,
-    combine_commands, skip_if_exists
-)
-import setuptools
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+from jupyter_packaging import get_data_files
+from setuptools import setup
+import pathlib
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-# The name of the project
-name="jupyterlab_markup"
-
-# Get our version
-with open(os.path.join(HERE, "package.json")) as f:
-    version = json.load(f)["version"]
-
-lab_path = os.path.join(HERE, name, "labextension")
-
-# Representative files that should exist after a successful build
-jstargets = [
-    os.path.join(lab_path, "package.json"),
-]
-
-package_data_spec = {
-    name: [
-        "*"
-    ]
-}
-
-labext_name = "@agoose77/jupyterlab-markup"
-
+# Add labextension target to data_files
+source_path = pathlib.Path("jupyterlab_markup") / "labextension"
+dest_path = pathlib.PurePath("share") / "jupyter" / "labextensions" / "@agoose77" / "jupyterlab-markup"
 data_files_spec = [
-    ("share/jupyter/labextensions/%s" % labext_name, lab_path, "**"),
-    ("share/jupyter/labextensions/%s" % labext_name, HERE, "install.json"),("etc/jupyter/jupyter_server_config.d",
-     "jupyter-config", "jupyterlab_markup.json"),
-
+    (dest_path, source_path.absolute(), "**"),
 ]
 
-cmdclass = create_cmdclass("jsdeps",
-    package_data_spec=package_data_spec,
-    data_files_spec=data_files_spec
-)
-
-js_command = combine_commands(
-    install_npm(HERE, build_cmd="build:prod", npm=["jlpm"]),
-    ensure_targets(jstargets),
-)
-
-is_repo = os.path.exists(os.path.join(HERE, ".git"))
-if is_repo:
-    cmdclass["jsdeps"] = js_command
-else:
-    cmdclass["jsdeps"] = skip_if_exists(jstargets, js_command)
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setup_args = dict(
-    name=name,
-    version=version,
-    url="https://github.com/agoose77/jupyterlab-markup.git",
-    author="Angus Hollands",
-    description="Additional markdown rendering support in markdown.",
-    long_description= long_description,
-    long_description_content_type="text/markdown",
-    cmdclass= cmdclass,
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "jupyterlab>=3.0.0rc13,==3.*",
-    ],
-    zip_safe=False,
-    include_package_data=True,
-    python_requires=">=3.6",
-    license="BSD-3-Clause",
-    platforms="Linux, Mac OS X, Windows",
-    keywords=["Jupyter", "JupyterLab", "JupyterLab3"],
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Framework :: Jupyter",
-    ],
-)
-
-
-if __name__ == "__main__":
-    setuptools.setup(**setup_args)
+setup(data_files=get_data_files(data_files_spec))
